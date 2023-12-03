ac5219d4a6da1f2dd5628fd0ebd3616ffcee434e
lutfinoor muhamad <lutfinoor.muhamad@gmail.com>
2023-12-03 13:58:40 +0700

    edit dotenv statement & install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 
-const supabaseUrl = "https://etfbrqovztmxdtuxzisa.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0ZmJycW92enRteGR0dXh6aXNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE0OTUwMjQsImV4cCI6MjAxNzA3MTAyNH0.ellzY-jm6K69CrW_CWknQMcZMhm_cezXNiXLIuN8EmY";
-
-// process.env.SUPABASE_KEY
+const supabaseUrl = "https://etfbrqovztmxdtuxzisa.supabase.co"
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 function App() {
@@ -23,7 +21,7 @@ function App() {
   return (
     <ul>
       {countries.map((country) => (
-        <li key={country.name}>{country.name}</li>
+        <li key={country.id}>{country.name} - {country.capital}</li>
       ))}
     </ul>
   );
